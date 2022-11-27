b87d2eb5016c5c274fe89c9391282825551767bd
Tom Rossner <tomrossner2@gmail.com>
2022-11-28 01:52:34 +0200

    Cleanup

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 let richPeople = json_arr;
-const container = document.querySelector(".grid-container");
+const container = document.querySelector(".flex-container");
 
-function createCards(){
+const createCards = () => {
     container.innerHTML = "";
-    if(richPeople.length === 0){
-        container.innerHTML = `Nothing to show`;
+    if (!richPeople.length) {
+        container.innerHTML = `No data`;
         container.style.fontSize = "2rem";
         container.style.fontStyle = "italic";
         container.style.color = "rgba(200, 200, 200, 0.2)";
@@ -14,15 +14,15 @@ function createCards(){
         reloadBtn.innerText = "Reload Page";
         reloadBtn.style.display = "block";
         container.appendChild(reloadBtn);
-        reloadBtn.addEventListener("click", () => {location.reload()});
+        reloadBtn.addEventListener("click", () => window.location.reload());
     }
-    richPeople.map(people => {
+    else richPeople.map((people) => {
         const newCard = document.createElement("div");
         const cardImg = document.createElement("img");
         const cardName = document.createElement("h3");
         const cardWorth = document.createElement("p");
         const cardSource = document.createElement("p");
-        newCard.className = "grid-item";
+        newCard.className = "flex-item";
         cardName.className = "detail name";
         cardWorth.className = "detail worth";
         cardSource.className = "detail source";
@@ -41,10 +41,10 @@ function createCards(){
     })
 }
 
-function removeCard(event){
+const removeCard = (event) => {
     const card = event.target.closest("[data-name]").dataset.name;
     richPeople = richPeople.filter((people) => people.name !== card);
-    createCards()
+    createCards();
 }
 
 createCards();
